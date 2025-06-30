fe2df747bb727582f068e9f1d3ff12b660636105
Ebey Joe Regi <ebeyjoeregi13@gmail.com>
2025-07-01 00:03:43 +0530

    collabration page update

--- src/pages/member/MemberCollaborationProjects.jsx
+++ src/pages/member/MemberCollaborationProjects.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import '../../styles//MemberCollaborationProjects.css';
+import '../../styles/collaborationProjects.css';
 
 export default function MemberCollaborationProjects() {
   const navigate = useNavigate();
